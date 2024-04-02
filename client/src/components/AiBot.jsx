a06548980b175336bbc495654a03a3af81925a7d
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-04-03 02:54:49 +0300

    Added AI bot component

--- client/src/components/AiBot.jsx
+++ client/src/components/AiBot.jsx
@@ -1,26 +1,12 @@
+/* eslint-disable no-unused-vars */
 import Search from "../../public/Search";
 
 const HeaderMessages = () => {
   return (
-    <div className="header-messages absolute top-[100%] right-[5%] z-50 w-[460px] duration-0.3 bg-primary py-[20px] px-4 hidden flex-col items-center justify-start gap-5">
-      <h3 className="font-Montagu text-[35px] text-white">Messages</h3>
-      <div className="w-full flex justify-between items-center gap-4">
-        <div className="basis-1/3 cursor-pointer flex items-center justify-between py-1 pl-[10px] pr-[8px] rounded bg-white">
-          <p className=" font-Montagu text-[20px] text-primary">Newest</p>
-        </div>
-        <div className="messages-search basis-2/3 h-[38px] relative cursor-pointer">
-          <input
-            type="text"
-            name="messages-search"
-            id="messages-search"
-            placeholder="search"
-            className="absolute h-full w-full top-0 left-0 rounded bg-white pl-[15px] pr-[35px] outline-none border-none text-[20px] text-primary"
-          />
-          <div className="absolute right-1 top-1/2 -translate-y-1/2 w-[30px] h-[25px]">
-            <Search wth="100%" hth="100%" fill="#595959" />
-          </div>
-        </div>
-      </div>
+    <div className="ai-bot absolute bottom-[120%] right-[50%] z-50 w-[460px] duration-0.3 bg-primary py-[20px] px-4 hidden flex-col items-center justify-start ">
+      <h3 className="font-Montagu text-[35px] text-white">AI Bot</h3>
+      <p className="font-Montagu  text-white">Chose a topic</p>
+
       <div className="messages-box w-full border-t border-white flex flex-col items-center justify-start gap-3 py-3">
         <div className="w-full flex justify-between items-center gap-2 rounded py-1 px-2 hover:bg-secondary cursor-pointer duration-0.3">
           <img
@@ -36,9 +22,6 @@ const HeaderMessages = () => {
               Lorem ipsum dolor sit
             </p>
           </div>
-          <p className="basis-1/5 font-Montagu text-white text-[14px] text-left">
-            12.01.2023
-          </p>
         </div>
         <div className="w-full flex justify-between items-center gap-2 rounded py-1 px-2 hover:bg-secondary cursor-pointer duration-0.3">
           <img
@@ -54,9 +37,6 @@ const HeaderMessages = () => {
               Lorem ipsum dolor sit
             </p>
           </div>
-          <p className="basis-1/5 font-Montagu text-white text-[14px] text-left">
-            12.01.2023
-          </p>
         </div>
         <div className="w-full flex justify-between items-center gap-2 rounded py-1 px-2 hover:bg-secondary cursor-pointer duration-0.3">
           <img
@@ -72,9 +52,6 @@ const HeaderMessages = () => {
               Lorem ipsum dolor sit
             </p>
           </div>
-          <p className="basis-1/5 font-Montagu text-white text-[14px] text-left">
-            12.01.2023
-          </p>
         </div>
       </div>
     </div>
--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,8 +1,49 @@
-import QuestionMark from '../../public/QuestionMark';
+/* eslint-disable no-unused-vars */
+
+import QuestionMark from "../../public/QuestionMark";
+import { useState, useEffect } from "react";
+import AiBot from "./AiBot";
 
 const Questions = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMessages = () => {
+    const messagesBox = document.querySelector(".ai-bot");
+    if (windowWidth >= 768 && messagesBox.classList.contains("hidden")) {
+      messagesBox.classList.remove("hidden");
+      messagesBox.classList.add("flex");
+    } else {
+      messagesBox.classList.remove("flex");
+      messagesBox.classList.add("hidden");
+    }
+  };
+
+  const hideMessages = () => {
+    const messagesBox = document.querySelector(".ai-bot");
+    if (windowWidth < 768 && messagesBox.classList.contains("flex")) {
+      messagesBox.classList.remove("flex");
+      messagesBox.classList.add("hidden");
+    }
+  };
+
   return (
-    <div className="question fixed bottom-[100px] sm:bottom-10 right-5 w-[63px] h-[63px] bg-primary flex items-center justify-center p-5 border border-white hover:border-secondary duration-0.3 rounded-full z-[100] cursor-pointer">
+    <div
+      className="question fixed bottom-[100px] sm:bottom-10 right-5 w-[63px] h-[63px] bg-primary flex items-center justify-center p-5 border border-white hover:border-secondary duration-0.3 rounded-full z-[100] cursor-pointer"
+      onClick={toggleMessages}
+    >
+      <AiBot />
       <QuestionMark wth="100%" hth="100%" fill="white" />
     </div>
   );
