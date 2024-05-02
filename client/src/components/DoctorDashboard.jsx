99efdd70ca796019f12a7664c754390e0ad93b3b
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-05-02 21:54:22 +0300

    Add doctor dashboard components with useAuth and state for courses data

--- client/src/components/DoctorDashboard.jsx
+++ client/src/components/DoctorDashboard.jsx
@@ -1,10 +1,46 @@
+/* eslint-disable no-unused-vars */
 import CurrentCourses from "./dashboard/CurrentCourses";
 import Shedule from "./dashboard/Shedule";
 import { motion } from "framer-motion";
 import { fadeIn } from "../motion/motion";
+import useAuth from "../hooks/useAuth";
+import { useEffect, useState } from "react";
+import api from "../utils/Request";
 
 const DoctorDashboard = () => {
   const admin = sessionStorage.getItem("admin");
+  const { token } = useAuth();
+  const [response, setResponse] = useState(null);
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/student/current", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setResponse(response.data); // Assuming the data you want is in the response's 'data' field
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const fetchCourses = async () => {
+      const courses = await api.get(`/course/list/mine?filter=current`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCourses(courses.data);
+    };
+
+    fetchCourses();
+
+    fetchData();
+  }, [token]);
+
   return (
     <div className="dashboard relative">
       <motion.h2
@@ -17,8 +53,8 @@ const DoctorDashboard = () => {
         Welcome <span className="text-secondary">{admin}</span>!
       </motion.h2>
       <div className="container">
-        <Shedule></Shedule>
-        <CurrentCourses></CurrentCourses>
+        <Shedule courses={courses}></Shedule>
+        <CurrentCourses courses={courses}></CurrentCourses>
       </div>
     </div>
   );
